fd9efc6a4807f45eb1ffcfa981bea7582e175705
Sebastian Köhler <mail@s-koehler.eu>
2023-12-24 17:44:40 +0100

    cli project of rust bootcamp

---
+++ cli-project/src/models.rs
@@ -0,0 +1,87 @@
+use std::{collections::HashMap, fmt::Display};
+
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NavigateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Open => write!(f, "OPEN"),
+            Self::InProgress => write!(f, "IN PROGRESS"),
+            Self::Resolved => write!(f, "RESOLVED"),
+            Self::Closed => write!(f, "Closed"),
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open and the stories should be an empty vector
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![],
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
+pub struct DBState {
+    // This struct represents the entire db state which includes the last_item_id, epics, and stories
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
+}
---
+++ cli-project/src/navigator.rs
@@ -0,0 +1,301 @@
+use anyhow::{anyhow, Result, Context, Ok};
+use std::{rc::Rc, ops::Deref};
+
+use crate::{ui::{Page, HomePage, EpicDetail, StoryDetail, Prompts}, db::JiraDatabase, models::Action};
+
+pub struct Navigator {
+    pages: Vec<Box<dyn Page>>,
+    prompts: Prompts,
+    db: Rc<JiraDatabase>
+}
+
+impl Navigator {
+    pub fn new(db: Rc<JiraDatabase>) -> Self {
+        let homepage = HomePage{
+            db: db.clone(),
+        };
+        Self {
+            pages: vec![Box::new(homepage)],
+            prompts: Prompts::new(),
+            db: db,
+        }
+    }
+
+    pub fn get_current_page(&self) -> Option<&Box<dyn Page>> {
+        self.pages.last()
+    }
+
+    pub fn handle_action(&mut self, action: Action) -> Result<()> {
+        match action {
+            Action::NavigateToEpicDetail { epic_id } => {
+                // create a new EpicDetail instance and add it to the pages vector
+                let epic_detail_page = EpicDetail{
+                    epic_id: epic_id,
+                    db: Rc::clone(&self.db),
+                };
+                self.pages.push(Box::new(epic_detail_page));
+            }
+            Action::NavigateToStoryDetail { epic_id, story_id } => {
+                // create a new StoryDetail instance and add it to the pages vector
+                let story_detail_page = StoryDetail{
+                    epic_id,
+                    story_id,
+                    db: Rc::clone(&self.db),
+                };
+                self.pages.push(Box::new(story_detail_page));
+            }
+            Action::NavigateToPreviousPage => {
+                // remove the last page from the pages vector
+                self.pages.pop();
+            }
+            Action::CreateEpic => {
+                // prompt the user to create a new epic and persist it in the database
+                let create_epic_prompt = self.prompts.create_epic.deref();
+                let epic = create_epic_prompt();
+                self.db.create_epic(epic).with_context(|| anyhow!("could not create epic"))?;
+            }
+            Action::UpdateEpicStatus { epic_id } => {
+                // prompt the user to update status and persist it in the database
+                //let db_state = self.db.read_db()?;
+
+                let update_epic_prompt = self.prompts.update_status.deref();
+                let new_status = update_epic_prompt().with_context(|| anyhow!("no valid status given"))?;
+                self.db.update_epic_status(epic_id, new_status).with_context(|| anyhow!("could not update epic status"))?;
+            }
+            Action::DeleteEpic { epic_id } => {
+                // prompt the user to delete the epic and persist it in the database
+                let delete_epic_prompt = self.prompts.delete_epic.deref();
+                let delete = delete_epic_prompt();
+                if delete {
+                    self.db.delete_epic(epic_id).with_context(|| anyhow!("could not delete epic"))?;
+                }
+            }
+            Action::CreateStory { epic_id } => {
+                // prompt the user to create a new story and persist it in the database
+                let create_story_prompt = self.prompts.create_story.deref();
+                let story = create_story_prompt();
+                self.db.create_story(story, epic_id).with_context(|| anyhow!("could not create story"))?;
+            }
+            Action::UpdateStoryStatus { story_id } => {
+                // prompt the user to update status and persist it in the database
+                let update_story_status_prompt = self.prompts.update_status.deref();
+                let status = update_story_status_prompt().with_context(|| anyhow!("could not parse status"))?;
+                self.db.update_story_status(story_id, status).with_context(|| anyhow!("could not update story status"))?;
+            }
+            Action::DeleteStory { epic_id, story_id } => {
+                // prompt the user to delete the story and persist it in the database
+                let delete_story_prompt = self.prompts.delete_story.deref();
+                let delete = delete_story_prompt();
+                if delete {
+                    self.db.delete_story(epic_id, story_id).with_context(|| anyhow!("could not delete story"))?;
+                }
+            }
+            Action::Exit => {
+                // remove all pages from the pages vector
+                self.pages = vec![];
+            },
+        }
+
+        Ok(())
+    }
+
+    // Private functions used for testing
+
+    fn get_page_count(&self) -> usize {
+        self.pages.len()
+    }
+
+    fn set_prompts(&mut self, prompts: Prompts) {
+        self.prompts = prompts;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{db::test_utils::MockDB, models::{Epic, Status, Story}};
+    use super::*;
+
+    #[test]
+    fn should_start_on_home_page() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+        let nav = Navigator::new(db);
+
+        assert_eq!(nav.get_page_count(), 1);
+
+        let current_page = nav.get_current_page().unwrap();
+        let home_page = current_page.as_any().downcast_ref::<HomePage>();
+
+        assert_eq!(home_page.is_some(), true);
+    }
+
+    #[test]
+    fn handle_action_should_navigate_pages() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+
+        let mut nav = Navigator::new(db);
+        
+        nav.handle_action(Action::NavigateToEpicDetail { epic_id: 1 }).unwrap();
+        assert_eq!(nav.get_page_count(), 2);
+
+        let current_page = nav.get_current_page().unwrap();
+        let epic_detail_page = current_page.as_any().downcast_ref::<EpicDetail>();
+        assert_eq!(epic_detail_page.is_some(), true);
+
+        nav.handle_action(Action::NavigateToStoryDetail { epic_id: 1, story_id: 2 }).unwrap();
+        assert_eq!(nav.get_page_count(), 3);
+
+        let current_page = nav.get_current_page().unwrap();
+        let story_detail_page = current_page.as_any().downcast_ref::<StoryDetail>();
+        assert_eq!(story_detail_page.is_some(), true);
+
+        nav.handle_action(Action::NavigateToPreviousPage).unwrap();
+        assert_eq!(nav.get_page_count(), 2);
+
+        let current_page = nav.get_current_page().unwrap();
+        let epic_detail_page = current_page.as_any().downcast_ref::<EpicDetail>();
+        assert_eq!(epic_detail_page.is_some(), true);
+
+        nav.handle_action(Action::NavigateToPreviousPage).unwrap();
+        assert_eq!(nav.get_page_count(), 1);
+
+        let current_page = nav.get_current_page().unwrap();
+        let home_page = current_page.as_any().downcast_ref::<HomePage>();
+        assert_eq!(home_page.is_some(), true);
+
+        nav.handle_action(Action::NavigateToPreviousPage).unwrap();
+        assert_eq!(nav.get_page_count(), 0);
+
+        nav.handle_action(Action::NavigateToPreviousPage).unwrap();
+        assert_eq!(nav.get_page_count(), 0);
+    }
+
+    #[test]
+    fn handle_action_should_clear_pages_on_exit() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+
+        let mut nav = Navigator::new(db);
+        
+        nav.handle_action(Action::NavigateToEpicDetail { epic_id: 1 }).unwrap();
+        nav.handle_action(Action::NavigateToStoryDetail { epic_id: 1, story_id: 2 }).unwrap();
+        nav.handle_action(Action::Exit).unwrap();
+
+        assert_eq!(nav.get_page_count(), 0);
+    }
+
+    #[test]
+    fn handle_action_should_handle_create_epic() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+
+        let mut nav = Navigator::new(Rc::clone(&db));
+
+        let mut prompts = Prompts::new();
+        prompts.create_epic = Box::new(|| Epic::new("name".to_owned(), "description".to_owned()));
+
+        nav.set_prompts(prompts);
+        
+        nav.handle_action(Action::CreateEpic).unwrap();
+
+        let db_state = db.read_db().unwrap();
+        assert_eq!(db_state.epics.len(), 1);
+
+        let epic = db_state.epics.into_iter().next().unwrap().1;
+        assert_eq!(epic.name, "name".to_owned());
+        assert_eq!(epic.description, "description".to_owned());
+    }
+
+    #[test]
+    fn handle_action_should_handle_update_epic() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+        let epic_id = db.create_epic(Epic::new("".to_owned(), "".to_owned())).unwrap();
+
+        let mut nav = Navigator::new(Rc::clone(&db));
+
+        let mut prompts = Prompts::new();
+        prompts.update_status = Box::new(|| Some(Status::InProgress));
+
+        nav.set_prompts(prompts);
+        
+        nav.handle_action(Action::UpdateEpicStatus { epic_id }).unwrap();
+
+        let db_state = db.read_db().unwrap();
+        assert_eq!(db_state.epics.get(&epic_id).unwrap().status, Status::InProgress);
+    }
+
+    #[test]
+    fn handle_action_should_handle_delete_epic() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+        let epic_id = db.create_epic(Epic::new("".to_owned(), "".to_owned())).unwrap();
+
+        let mut nav = Navigator::new(Rc::clone(&db));
+
+        let mut prompts = Prompts::new();
+        prompts.delete_epic = Box::new(|| true);
+
+        nav.set_prompts(prompts);
+        
+        nav.handle_action(Action::DeleteEpic { epic_id }).unwrap();
+
+        let db_state = db.read_db().unwrap();
+        assert_eq!(db_state.epics.len(), 0);
+    }
+
+    #[test]
+    fn handle_action_should_handle_create_story() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+        let epic_id = db.create_epic(Epic::new("".to_owned(), "".to_owned())).unwrap();
+
+        let mut nav = Navigator::new(Rc::clone(&db));
+
+        let mut prompts = Prompts::new();
+        prompts.create_story = Box::new(|| Story::new("name".to_owned(), "description".to_owned()));
+
+        nav.set_prompts(prompts);
+        
+        nav.handle_action(Action::CreateStory { epic_id }).unwrap();
+
+        let db_state = db.read_db().unwrap();
+        assert_eq!(db_state.stories.len(), 1);
+
+        let story = db_state.stories.into_iter().next().unwrap().1;
+        assert_eq!(story.name, "name".to_owned());
+        assert_eq!(story.description, "description".to_owned());
+    }
+
+    #[test]
+    fn handle_action_should_handle_update_story() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+        let epic_id = db.create_epic(Epic::new("".to_owned(), "".to_owned())).unwrap();
+        let story_id = db.create_story(Story::new("".to_owned(), "".to_owned()), epic_id).unwrap();
+
+        let mut nav = Navigator::new(Rc::clone(&db));
+
+        let mut prompts = Prompts::new();
+        prompts.update_status = Box::new(|| Some(Status::InProgress));
+
+        nav.set_prompts(prompts);
+        
+        nav.handle_action(Action::UpdateStoryStatus { story_id }).unwrap();
+
+        let db_state = db.read_db().unwrap();
+        assert_eq!(db_state.stories.get(&story_id).unwrap().status, Status::InProgress);
+    }
+
+    #[test]
+    fn handle_action_should_handle_delete_story() {
+        let db = Rc::new(JiraDatabase { database: Box::new(MockDB::new()) });
+        let epic_id = db.create_epic(Epic::new("".to_owned(), "".to_owned())).unwrap();
+        let story_id = db.create_story(Story::new("".to_owned(), "".to_owned()), epic_id).unwrap();
+
+        let mut nav = Navigator::new(Rc::clone(&db));
+
+        let mut prompts = Prompts::new();
+        prompts.delete_story = Box::new(|| true);
+
+        nav.set_prompts(prompts);
+        
+        nav.handle_action(Action::DeleteStory { epic_id, story_id }).unwrap();
+
+        let db_state = db.read_db().unwrap();
+        assert_eq!(db_state.stories.len(), 0);
+    }
+}
